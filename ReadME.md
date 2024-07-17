3587f7930a7bf00e9e970ac89fe18f70bfda6359
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-07-17 18:52:33 +0530

    initial project setup completed --connection to mongodb established

---
+++ ReadME.md
@@ -0,0 +1,3 @@
+# Project Management Tool Backend
+
+# This is a backend for a project management application that is developed using Express.JS and MongoDB.
---
+++ app.js
@@ -0,0 +1,8 @@
+// Importing the express library
+const express = require("express");
+
+// Creating an express application
+const app = express();
+
+// Export the express app
+module.exports = app;
