85d9a1b8e9416efd153689a9ff37734ba133d67d
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-07-26 16:38:19 +0530

    added CORS for resource sharing

--- app.js
+++ app.js
@@ -19,9 +19,22 @@ const notificationRouter = require("./routes/notificationRoutes");
 // Importing the morgan library to log requests
 const morgan = require("morgan");
 
+// Importing the cors library
+const cors = require("cors");
+
 // Importing the cookie parser library
 const cookieParser = require("cookie-parser");
+
+// Custom error handler middleware
 const errorHandler = require("./utils/Error");
+
+// Adding the cors middleware to allow cross-origin requests
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Adding the notification job
 require("./jobs/NotificationJob");
