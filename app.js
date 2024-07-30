6020cae546598a5b210a6a50eac920fe1179ac65
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-07-30 21:55:16 +0530

    fixed cors

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const app = express();
 // Adding the cors middleware to allow cross-origin requests
 app.use(
   cors({
-    origin: "https://pro-manager-tool.netlify.app/",
+    origin: "https://pro-manager-tool.netlify.app",
     credentials: true,
   })
 );
